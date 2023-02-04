2524349b015000ba4101c64836e4adc7b8bc4a92
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2023-02-04 19:28:58 +0330

    Server Side Rendering (SSR)

---
+++ pages/blogs/index.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const BlogsList = ({ blogs }) => {
+  return (
+    <div>
+      <h1>Blog List Page</h1>
+      <div>
+        {blogs.map((blog) => {
+          return (
+            <h2>
+              Title: {blog.title} - Category: {blog.category}
+            </h2>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default BlogsList;
+
+export async function getServerSideProps(context) {
+  const { data } = await axios.get("http://localhost:4000/blogs");
+  return {
+    props: {
+      blogs: data,
+    },
+  };
+}
