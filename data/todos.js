040ba636a524389e7f6e05935fae45ef3df070d4
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2023-02-05 16:24:23 +0330

    API Routes end point

---
+++ data/todos.js
@@ -0,0 +1,14 @@
+export const todos = [
+  {
+    id: 1,
+    title: "React.js",
+  },
+  {
+    id: 2,
+    title: "Node.js",
+  },
+  {
+    id: 3,
+    title: "Next.js",
+  },
+];
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,61 @@
-const HomePage = () => {
+import { CheckIcon, PencilIcon, TrashIcon } from "@heroicons/react/24/outline";
+const Home = () => {
   return ( 
-    <div className="bg-red-200 text-pink-600">This is HomePage</div>
+    <div className="bg-gray-50 min-h-screen">
+      <nav className=" w-full bg-white shadow-sm flex justify-center py-4 mb-6">
+        <h1 className="font-bold">TodoList App using Next.js & TailwindCSS</h1>
+      </nav>
+      <div className="container p-2 xl:max-w-screen-xl mx-auto">
+        <section className="flex items-center justify-center">
+          <div className="w-full max-w-screen-md bg-white p-2 md:p-4 rounded-xl">
+            <div className="flex items-center justify-between border border-gray-100 mb-4 p-3 md:p-4 rounded-xl">
+              <span>Task 1</span>
+              <div className="flex gap-x-3 items-center">
+                <button className="">
+                  <CheckIcon className="w-6 h-6 stroke-green-400" />
+                </button>
+                <button className="">
+                  <TrashIcon className="w-6 h-6 stroke-red-400" />
+                </button>
+                <button>
+                  <PencilIcon className="w-6 h-6 stroke-blue-400" />
+                </button>
+              </div>
+            </div>
+            <div className="flex items-center justify-between border border-gray-100 mb-4 p-3 md:p-4 rounded-xl">
+              <span>Task 1</span>
+              <div className="flex gap-x-3 items-center">
+                <button className="">
+                  <CheckIcon className="w-6 h-6 stroke-green-400" />
+                </button>
+                <button className="">
+                  <TrashIcon className="w-6 h-6 stroke-red-400" />
+                </button>
+                <button>
+                  <PencilIcon className="w-6 h-6 stroke-blue-400" />
+                </button>
+              </div>
+            </div>
+            <div className="flex items-center justify-between border border-gray-100 mb-4 p-3 md:p-4 rounded-xl">
+              <span>Task 1</span>
+              <div className="flex gap-x-3 items-center">
+                <button className="">
+                  <CheckIcon className="w-6 h-6 stroke-green-400" />
+                </button>
+                <button className="">
+                  <TrashIcon className="w-6 h-6 stroke-red-400" />
+                </button>
+                <button>
+                  <PencilIcon className="w-6 h-6 stroke-blue-400" />
+                </button>
+              </div>
+            </div>
+          </div>
+          
+        </section>
+      </div>
+    </div>
    );
 }
  
-export default HomePage;
+export default Home;
