e037b768682125d9be6f319fea2225996dcd8d90
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2023-02-07 14:38:29 +0330

    Edit Todo

--- components/todos/TodoList.js
+++ components/todos/TodoList.js
@@ -10,7 +10,9 @@ const TodoList = ({ todos, onDelete }) => {
             key={todo._id}
             className="flex items-center justify-between border border-gray-100 mb-4 p-3 md:p-4 rounded-xl"
           >
-            <Link href={`/todos/${todo._id}`}><span>{todo.title}</span></Link>
+            <Link href={`/todos/${todo._id}`}>
+              <span>{todo.title}</span>
+            </Link>
             <div className="flex gap-x-3 items-center">
               <button>
                 <CheckIcon className="w-6 h-6 stroke-green-400" />
@@ -18,9 +20,9 @@ const TodoList = ({ todos, onDelete }) => {
               <button onClick={() => onDelete(todo._id)}>
                 <TrashIcon className="w-6 h-6 stroke-red-400" />
               </button>
-              <button>
+              <Link href={`todos/edit/${todo._id}`}>
                 <PencilIcon className="w-6 h-6 stroke-blue-400" />
-              </button>
+              </Link>
             </div>
           </div>
         );
--- pages/api/todos/[todoId].js
+++ pages/api/todos/[todoId].js
@@ -14,6 +14,14 @@ export default async function handler(req, res) {
   } else if (method === "GET") {
     const todo = await getOneTodo(query);
     return res.status(200).json({ message: "todo added", todo });
+  }else if (method === "PUT") {
+    const { body } = req;
+    const todo = await Todo.findById(query.todoId);
+    todo.title = body.todo.title;
+    todo.description = body.todo.description;
+    await todo.save();
+    const todos = await Todo.find({});
+    return res.status(200).json({ message: "todo edited successfully", todos });
   }
 }
 
