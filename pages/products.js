0d471736a7e31d4cd2761159ef8ae87c8bb842f5
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2023-03-03 13:13:23 +0330

    placeholder and loading in Image component

--- pages/products.js
+++ pages/products.js
@@ -1,13 +1,35 @@
 import Image from "next/image";
+import myImage from "../public/images/1.jpg";
 
 const Products = () => {
-  return ["1","2","3","4"].map((path)=>{
-    return (
+  return (
+    <div>
       <div>
-        <Image src={`/images/${path}.jpg`} width="800" height="400" quality={30} />
+        <Image
+          src={myImage}
+          width="800"
+          height="400"
+          quality={30}
+          placeholder="blur"
+        />
       </div>
-    )
-  });
-}
- 
+      {["1", "2", "3", "4"].map((path) => {
+        return (
+          <div>
+            <Image
+              src={`/images/${path}.jpg`}
+              width="800"
+              height="400"
+              quality={30}
+              placeholder="blur"
+              blurDataURL={`/images/${path}.jpg`}
+              loading="lazy"
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
 export default Products;
